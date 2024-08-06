0386a5cbf1d7d10d1e2cc7223cfd1be66a6156e7
whelew <157028660+whelew@users.noreply.github.com>
2024-08-06 12:45:19 +0000

    inital new_game function created

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def new_game():
+    print("Welcome to RollPG.")
+    print("To begin your new adventure we need a name.")
+    character_name = input("What is your characters name:")
+    return character_name
+
+def main():
+    character_name = new_game()
+    print(character_name)
+
+main()
