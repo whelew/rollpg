541b8152d9f6994a6649b74a945fbe35e2110d85
whelew <157028660+whelew@users.noreply.github.com>
2024-08-06 14:13:36 +0000

    Added while loop to select_race function

--- run.py
+++ run.py
@@ -9,17 +9,23 @@ def new_game():
     return character_name
 
 def select_race():
-    pick_race = input("Now select a race, are you a Human, Dwarf or an Elf:\n")
-    race = pick_race.lower()
-    if race == "dwarf":
-        race = "dwarf"
-    elif race == "human":
-        race = "human"
-    elif race == "elf":
-        race = "elf"
-    else:
-        print("I've not heard of that race before.. please select one from the list.\n")
-        select_race()
+
+    while True:
+        pick_race = input("Now select a race, are you a Human, Dwarf or an Elf:\n")
+        race = pick_race.lower()
+
+        if race == "dwarf":
+            race = "dwarf"
+            break
+        elif race == "human":
+            race = "human"
+            break
+        elif race == "elf":
+            race = "elf"
+            break
+        else:
+            print("I've not heard of that race before.. please select one from the list.\n")
+            select_race()
     return race
 
 def main():
