9b8585ddb25207705d636d7a6f3d8780873d9ea7
whelew <157028660+whelew@users.noreply.github.com>
2024-08-08 11:35:51 +0000

    Added docstring to select_race function, asked user to input 1, 2 or 3

--- run.py
+++ run.py
@@ -11,21 +11,26 @@ def new_game():
 def select_race():
 
     while True:
-        pick_race = input("Now select a race, are you a Human, Dwarf or an Elf:\n")
-        race = pick_race.lower()
+        """
+        Sets race to 1 of 3 options, using if elif statement to check 
+        user input if the user does not input 1, 2 or 3
+        the user will be told to select one from the list.
+        """
+        print("Now select a race, are you a Human, Dwarf or an Elf")
+        pick_race = input("Enter 1, 2, or 3:\n")
+        race = pick_race
 
-        if race == "dwarf":
-            race = "dwarf"
-            break
-        elif race == "human":
+        if race == "1":
             race = "human"
             break
-        elif race == "elf":
+        elif race == "2":
+            race = "dwarf"
+            break
+        elif race == "3":
             race = "elf"
             break
         else:
             print("I've not heard of that race before.. please select one from the list.\n")
-            select_race()
     return race
 
 def main():
