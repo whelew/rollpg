7f331ac5fd7f0f73776d6c4ddb5304d4b4754f67
whelew <157028660+whelew@users.noreply.github.com>
2024-08-08 12:48:50 +0000

    Character_name function checks to see if input contains integers, whitespace or empty

--- run.py
+++ run.py
@@ -1,12 +1,29 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import math
 
 def new_game():
     print("Welcome to RollPG.")
+        
+def character_name():
+    """
+    Set players name, checks to see if name contains an integer.
+    Checks to see if input is blank or contains white space.
+    Returns name if none of the above.
+    """
     print("To begin your new adventure we need a name.")
-    character_name = input("What is your characters name:")
-    return character_name
+    while True:
+        name = input("What is your characters name:")
+        if any(n.isdigit() for n in name):
+            print("You can't have a number in your name... please try again.")
+        elif name.isspace() == True:
+            print("You can't be called nothing...")
+        elif name == "":
+            print("You can't be called nothing...")
+        else:
+            break
+    return name   
 
 def select_race():
 
@@ -34,8 +51,9 @@ def select_race():
     return race
 
 def main():
-    character_name = new_game()
-    print(f'Yes... {character_name}, a very heroic name!\n')
+    new_game()
+    c_name = character_name()
+    print(f'Yes... {c_name}, a very heroic name!\n')
     race = select_race()
     print(f"A {race} you say...\n")
 
